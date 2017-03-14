104b1556981f8834a8c6a19ef88351e352fb3092
luizfmarins <luiz.f.marins@gmail.com>
2017-03-13 21:17:20 -0300

    7- HTTP - Add a Hero

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -9,6 +9,7 @@ import { Hero } from './hero';
 export class HeroService {
 
 	private heroesUrl = 'api/heroes'; 
+  private headers = new Headers({'Content-Type': 'application/json'});
 
 	constructor(private http: Http) { }
 
@@ -31,4 +32,22 @@ export class HeroService {
       .then(response => response.json().data as Hero)
       .catch(this.handleError);
   }
+
+  update(hero: Hero): Promise<Hero> {
+    const url = `${this.heroesUrl}/${hero.id}`;
+    return this.http
+      .put(url, JSON.stringify(hero), {headers: this.headers})
+      .toPromise()
+      .then(() => hero)
+      .catch(this.handleError);
+  }
+
+  create(name: string): Promise<Hero> {
+    return this.http
+      .post(this.heroesUrl, JSON.stringify({name: name}), {headers: this.headers})
+      .toPromise()
+      .then(res => res.json().data)
+      .catch(this.handleError);
+  }
+
 }
